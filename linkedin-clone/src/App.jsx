8a8dc8c85b3332b3ef84c786066b508a9006fc7d
Zoltan3do <baronemanuel05@gmail.com>
2024-09-09 17:51:30 +0200

    ScrollChangeMArginonAPp

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -1,13 +1,22 @@
 import MyNavbar from './components/Navbar/MyNavbar'
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
 
+  const [isScrollFromChild, setIsScrollFromChild] = useState(false);
+
+  // Funzione che riceve lo stato dal figlio
+  const handleScrollChange = (newScrollState) => {
+    setIsScrollFromChild(newScrollState);
+  };
+
+
   return (
     <>
-      <MyNavbar />
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?</div>
+      <MyNavbar onScrollChange={handleScrollChange}/>
+      <div className={!isScrollFromChild?"no-scroll-margin":"scroll-margin"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?</div>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?</div>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?</div>
       <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?</div>
