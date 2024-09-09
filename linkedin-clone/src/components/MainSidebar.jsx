bca4da14bf454a103222dc058204828475fdb1f6
Diana <dianadorojuc3@gmail.com>
2024-09-09 12:15:23 +0200

    scelta lingua

---
+++ linkedin-clone/src/components/MainSidebar.jsx
@@ -0,0 +1,12 @@
+import { Row, Col } from "react-bootstrap"
+import SidebarProfile from "./SidebarProfile"
+const MainSidebar = () =>{
+    return(
+       <Row>
+        <Col>
+        <SidebarProfile/>
+        </Col>
+       </Row>
+    )
+}
+export default MainSidebar
