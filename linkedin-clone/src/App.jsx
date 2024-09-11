dd28d87972687269ecdcb8ca1331141102df22ed
sudonona <80674485+sudonona@users.noreply.github.com>
2024-09-11 10:33:33 +0200

    adds

--- linkedin-clone/src/App.jsx
+++ linkedin-clone/src/App.jsx
@@ -1,19 +1,18 @@
-import MyNavbar from './components/Navbar/MyNavbar'
+import MyNavbar from './components/Navbar/MyNavbar';
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import MyFooter from './components/myFooter'
-import MyFooter from "./components/myFooter";
+import './App.css';
+import Profile from './components/Profile'; 
+import MyFooter from './components/MyFooter';
 
 function App() {
-
   const [isScrollFromChild, setIsScrollFromChild] = useState(false);
 
   // Funzione che riceve lo stato dal figlio
   const handleScrollChange = (newScrollState) => {
     setIsScrollFromChild(newScrollState);
   };
-
 
   return (
     <BrowserRouter>
@@ -22,21 +21,22 @@ function App() {
           path="/"
           element={
             <>
-              {/* <div className='animate__animated animate__hinge'> */}
-
               <header>
                 <MyNavbar onScrollChange={handleScrollChange} />
-                <div className={!isScrollFromChild ? "no-scroll-margin" : "scroll-margin"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?</div>
+                <div className={!isScrollFromChild ? "no-scroll-margin" : "scroll-margin"}>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo quasi sequi repudiandae error. Veritatis ullam at, facilis quod sint quasi omnis iusto voluptatibus facere accusamus repudiandae, soluta aspernatur temporibus?
+                </div>
               </header>
               <main>
-                <Profile />
+                <Profile /> 
               </main>
+              <MyFooter />
             </>
           }
         />
       </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
