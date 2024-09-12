b90117c1d285cf46122198ea8b3ce9e209f9448c
Zoltan3do <baronemanuel05@gmail.com>
2024-09-12 17:50:06 +0200

    Dinamicizzazione dei job links

--- linkedin-clone/src/components/Jobs/SingleJob.jsx
+++ linkedin-clone/src/components/Jobs/SingleJob.jsx
@@ -8,12 +8,12 @@ const SingleJob = ({ job }) => {
     return (
         <Card className="card mr-3 mb-3">
             <Card.Body>
-                <Card.Link className="jobtitle mt-2 text-decoration-none fw-semibold fs-5" href={job.url}>
+                <Link className="jobtitle mt-2 text-decoration-none fw-semibold fs-5" to={`/jobs-finder?search=${job.title}`}>
                     {job.title}
                     <span>&nbsp;
                         <small>({job.job_type})</small>
                     </span>
-                </Card.Link>
+                </Link>
                 <Link to={`/${job.company_name}`} className="text-decoration-none">
                     <Card.Subtitle className="mt-2  text-dark">
                         {job.company_name}
