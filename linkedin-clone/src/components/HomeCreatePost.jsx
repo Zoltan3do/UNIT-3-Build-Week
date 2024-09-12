aaa93e0ab748b4b62e82d36dc710b015fee53d24
Zoltan3do <baronemanuel05@gmail.com>
2024-09-12 15:18:31 +0200

    mb-4 removed

--- linkedin-clone/src/components/HomeCreatePost.jsx
+++ linkedin-clone/src/components/HomeCreatePost.jsx
@@ -48,7 +48,7 @@ const HomeCreatePost = () => {
     }, [dispatch]);
 
     return (
-        <div className="p-3 mb-4" style={{ marginTop: "100px" }}> {/* rimuovere il margin bottom */}
+        <div className="p-3" style={{ marginTop: "100px" }}>
             <Card className='p-3 rounded-3'>
                 <div className="d-flex align-items-center">
                     {user && user.image ? (
