5291b23321371b52faa9109bcbf3d7a28ae4f9ec
Zoltan3do <baronemanuel05@gmail.com>
2024-09-12 11:29:54 +0200

    single job adjustment

--- linkedin-clone/src/components/Jobs/SingleJob.jsx
+++ linkedin-clone/src/components/Jobs/SingleJob.jsx
@@ -5,30 +5,29 @@ import "./Jobs.css";
 
 
 const SingleJob = ({ job }) => {
-  return (
-    <Card className="card mr-3 mb-3">
-      <Card.Body>
-        <Card.Link className="jobtitle mt-2" href={job.url}>
-          {job.title}
-          <span>
-            <small>({job.job_type})</small>
-          </span>
-        </Card.Link>
-        <Card.Text></Card.Text>
-        <Link to={`/${job.company_name}`}>
-          <Card.Subtitle className="mt-2">
-            {job.company_name}
-          </Card.Subtitle>
-        </Link>
-        <Card.Text>{job.candidate_required_location}</Card.Text>
+    return (
+        <Card className="card mr-3 mb-3">
+            <Card.Body>
+                <Card.Link className="jobtitle mt-2 text-decoration-none fw-semibold fs-5" href={job.url}>
+                    {job.title}
+                    <span>&nbsp;
+                        <small>({job.job_type})</small>
+                    </span>
+                </Card.Link>
+                <Link to={`/${job.company_name}`} className="text-decoration-none">
+                    <Card.Subtitle className="mt-2  text-dark">
+                        {job.company_name}
+                    </Card.Subtitle>
+                </Link>
+                <Card.Text className="mb-0">{job.candidate_required_location}</Card.Text>
 
-        <Card.Text className="active">
-          <small className="ml-1">Actively recruiting</small>
-        </Card.Text>
+                <Card.Text className="active ">
+                    <small className="ml-1">Selezione attiva</small>
+                </Card.Text>
 
-        <Card.Text>{job.publication_date}</Card.Text>
-      </Card.Body>
-    </Card>
-  );
+                <Card.Text>{job.publication_date}</Card.Text>
+            </Card.Body>
+        </Card>
+    );
 };
 export default SingleJob;
